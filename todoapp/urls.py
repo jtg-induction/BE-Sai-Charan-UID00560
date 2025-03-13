ea5ba7e3661fa56c422a9ec98722fa971f4c8d8e
sai-charan-appana <venkata.charan@joshtechnologygroup.com>
2025-03-13 15:00:20 +0530

    Initial codebase

---
+++ todoapp/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+
+api_urls = [
+    path("todos/", include("todos.urls")),
+    path("", include("users.urls")),
+]
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include(api_urls)),
+]
