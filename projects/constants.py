b27997857b320cb3698ef86435ceebbfbf18a128
sai-charan-appana <venkata.charan@joshtechnologygroup.com>
2025-03-17 12:39:15 +0530

    [Exercise-4]: minor changes

--- projects/constants.py
+++ projects/constants.py
@@ -5,9 +5,9 @@ IN_PROGRESS = 1
 COMPLETED = 2
 
 PROJECT_STATUS = {
-    TO_BE_STARTED: "To Be Started",
-    IN_PROGRESS: "In Progress",
-    COMPLETED: "Completed",
+    TO_BE_STARTED: "to-be-started",
+    IN_PROGRESS: "in-progress",
+    COMPLETED: "completed",
 }
 
 
