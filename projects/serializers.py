4cf1750e14672d2b251543625ccb84f87c9b19fa
sai-charan-appana <venkata.charan@joshtechnologygroup.com>
2025-03-18 11:08:41 +0530

    [Exercise-7]: refactor serializer to use constants

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,21 +1,17 @@
 from rest_framework import serializers
 
 from commons.serializers import BaseModelSerializer
+from projects.constants import ProjectFields
 from projects.models import Project
 from users.serializers import CustomUserSerializerWithTodoStats
 
 
-class ProjectSerializer(BaseModelSerializer):
-
-    existing_member_count = serializers.IntegerField(read_only=True)
-    status = serializers.SerializerMethodField()
+class ProjectSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Project
-        fields = ["id", "name", "status", "existing_member_count", "max_members"]
-
-    def get_status(self, obj):
-        return obj.get_status_display()
+        fields = "__all__"
+        read_only_fields = [ProjectFields.ID.value]
 
 
 class ProjectSerializerWithReport(BaseModelSerializer):
--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,4 @@
-from django.contrib.auth import authenticate, get_user_model
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
